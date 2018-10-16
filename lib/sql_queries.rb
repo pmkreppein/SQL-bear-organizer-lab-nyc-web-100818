23c688a53b5543635720d9bbd8114421da130267
Peter Kreppein <pmkreppein@gmail.com>
2018-10-16 18:45:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,7 +4,7 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT bears.name FROM bears ORDER BY name"
  
 end
 
